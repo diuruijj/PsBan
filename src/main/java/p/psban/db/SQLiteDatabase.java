52346229a1d8ef5a60d6a111fd92047c14782826
Spinnin <123275059+Spinnin34@users.noreply.github.com>
2026-04-01 21:40:18 +0200

    Delete src/main/java/p/psban/db directory

--- src/main/java/p/psban/db/SQLiteDatabase.java
+++
@@ -1,9 +0,0 @@
-package p.psban.db;
-
-import p.psban.PsBanPlugin;
-
-public class SQLiteDatabase extends FlatFileDatabase {
-    public SQLiteDatabase(PsBanPlugin plugin) {
-        super(plugin, "psban-data.yml");
-    }
-}
